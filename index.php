a79917cf29d7374e620a41d93c06cc10c3a7b111
Md_Bayzid_mostafiz <bayazid416@gmail.com>
2025-02-18 12:09:28 +0600

    first

---
+++ index.php
@@ -0,0 +1,54 @@
+<!DOCTYPE html>
+<html>
+<head>
+    <title>Get User Input</title>
+</head>
+<body>
+
+
+<form class="first_form"method='post'>
+        <h3>Enter Your Value Here</h3>
+        <input type='number' name='num1' placeholder='Enter first number' required>
+        <input type='number' name='num2' placeholder='Enter second number' required>
+        <button type='submit'>Submit</button>
+</form>
+
+<?php
+if ($_SERVER["REQUEST_METHOD"] == "POST") {
+    // Store user inputs in variables
+    $x = $_POST['num1'];
+    $y = $_POST['num2'];
+
+    if($x==$y){
+        echo "<style>
+        form.first_form{
+            display: none
+        }
+      </style>";
+        echo "Correct<br>";
+        echo '<button><a href="https://codding.xiantxpress.com/">You want to try again?</a></button>';
+        
+        
+    }
+    else{
+       
+        echo "<style>
+        form.first_form{
+            display: none
+        }
+      </style>";
+        echo "<h3>The Value Is Not Equal Please Try Again</h3>";
+        echo "<form method='post'>
+        <input type='number' name='num1' placeholder='Enter first number' required>
+        <input type='number' name='num2' placeholder='Enter second number' required>
+        <button type='submit'>Submit</button>
+</form>";
+    }
+
+    // You can now use these variables for further processing
+}
+?>
+
+
+</body>
+</html>
