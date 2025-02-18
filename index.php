14a9bd46d86684e2adaf8edd69e37abc1cc10f6e
Md_Bayzid_mostafiz <bayazid416@gmail.com>
2025-02-18 18:07:24 +0600

    Your commit message here

--- index.php
+++ index.php
@@ -1,53 +1,69 @@
 <!DOCTYPE html>
-<html>
+<html lang="en">
 <head>
-    <title>Get User Input</title>
-</head>
+    <meta charset="UTF-8">
+    <meta name="viewport" content="width=device-width, initial-scale=1.0">
+    <title>Contact</title>
+                        <!-- bootstrap cdn include-->
+    <link href="https://cdn.jsdelivr.net/npm/bootstrap@5.3.3/dist/css/bootstrap.min.css" rel="stylesheet" integrity="sha384-QWTKZyjpPEjISv5WaRU9OFeRpok6YctnYmDr5pNlyT2bRjXh0JMhjY6hW+ALEwIH" crossorigin="anonymous">
+                            <!-- fontAwesome cdn include-->
+    <link rel="stylesheet" href="https://cdnjs.cloudflare.com/ajax/libs/font-awesome/6.7.2/css/all.min.css" integrity="sha512-Evv84Mr4kqVGRNSgIGL/F/aIDqQb7xQ2vcrdIwxfjThSH8CSR7PBEakCr51Ck+w+/U6swU2Im1vVX0SVk9ABhg==" crossorigin="anonymous" referrerpolicy="no-referrer" />
+                            <!-- Style sheet connect -->
+    <link rel="stylesheet" href="style.css">
+</head> 
 <body>
 
+    
+    <div class="container ">
+        <div class="row">
+            <header class="text-light text-center bg-black p-3"><h3>Contact Form</h3></header>
+        </div>
 
-<form class="first_form"method='post'>
-        <h3>Enter Your Value Here</h3>
-        <input type='number' name='num1' placeholder='Enter first number' required>
-        <input type='number' name='num2' placeholder='Enter second number' required>
-        <button type='submit'>Submit</button>
-</form> 
-<?php
-if ($_SERVER["REQUEST_METHOD"] == "POST") {
-    // Store user inputs in variables
-    $x = $_POST['num1'];
-    $y = $_POST['num2'];
 
-    if($x==$y){
-        echo "<style>
-        form.first_form{
-            display: none
-        }
-      </style>";
-        echo "Correct<br>";
-        echo '<button><a href="https://codding.xiantxpress.com/">You want to try again?</a></button>';
+
+        <div class="row ">
+            <div class="main col-12 d-flex align-items-center justify-content-center bg-dark">
+                <form action="" class="col-8">
+                                            
+                    <div class="row d-flex col-12 d-flex align-items-center justify-content-center">
+                        <div class="col-2 text-light"><p>First Name</p></div>
+                        <div class="col-6"><input class="col-12" type="text" name="first_name" ></div>
+                    </div>
+
+                    <div class="row d-flex col-12 d-flex align-items-center justify-content-center">
+                        <div class="col-2 text-light"><p>Last Name</p></div>
+                        <div class="col-6"><input class="col-12" type="text" name="lastname" ></div>
+                    </div>
+
+                    <div class="row d-flex col-12 d-flex align-items-center justify-content-center">
+                        <div class="col-2 text-light"><p>Phone Number</p></div>
+                        <div class="col-6"><input class="col-12" type="text" name="phone" ></div>
+                    </div>
+
+                    <div class="row d-flex col-12 d-flex align-items-center justify-content-center">
+                        <div class="col-2 text-light"><p>Email Address</p></div>
+                        <div class="col-6"><input class="col-12" type="text" name="email" ></div>
+                    </div>
+
+                    <div class="row d-flex col-12 d-flex align-items-center justify-content-center">
+                        <div class="col-2 text-light"><p>Message</p></div>
+                        <div class="col-6"><textarea class="col-12" name="message"></textarea></div>
+                    </div>
+                    <div class="submit col-12"><button class="p-3" type="submit" name="submit">Submit</button></div>
+                </form>
+            </div>
+        </div>
+
+
+
+
+        <div class="row">
+        <footer class="text-light text-center p-3 bg-black"><h3>This is Footer</h3></footer>
+        </div>
         
-        
-    }
-    else{
-       
-        echo "<style>
-        form.first_form{
-            display: none
-        }
-      </style>";
-        echo "<h3>The Value Is Not Equal Please Try Again</h3>";
-        echo "<form method='post'>
-        <input type='number' name='num1' placeholder='Enter first number' required>
-        <input type='number' name='num2' placeholder='Enter second number' required>
-        <button type='submit'>Submit</button>
-</form>";
-    }
-
-    // You can now use these variables for further processing
-}
-?>
+    </div>
 
 
+    <script src="https://cdn.jsdelivr.net/npm/bootstrap@5.3.3/dist/js/bootstrap.bundle.min.js" integrity="sha384-YvpcrYf0tY3lHB60NNkmXc5s9fDVZLESaAA55NDzOxhy9GkcIdslK1eN7N6jIeHz" crossorigin="anonymous"></script>
 </body>
 </html>
